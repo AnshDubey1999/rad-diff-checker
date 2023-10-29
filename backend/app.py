5474ca2794820d0b7ccc5ea03351a747ef46f1b0
Ansh Dubey <ansh@ualberta.ca>
2023-10-29 05:45:57 -0600

    feat: add SQLite model and update dependencies

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,45 @@
 from flask import Flask, jsonify, abort
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.exceptions import NotFound
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///templates.db'
+db = SQLAlchemy(app)
 
+
+class Template(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    template_text = db.Column(db.String, nullable=False)
+
+
+# Root route
 @app.route('/')
 def index():
-    return jsonify({'message': 'Hello World!'})
+    return jsonify(message="Hello, world!")
 
 
 @app.route('/get-template/<int:template_id>', methods=['GET'])
 def get_template(template_id):
-    pass
+    '''
+    Get a template by ID
+    args:
+        template_id: ID of the template to retrieve
+    '''
+    # Query the database for the template
+    template: Template = Template.query.get(template_id)
+
+    # If the template doesn't exist, return a 404 error
+    if not template:
+        abort(404, description=f"Template with ID {template_id} was not found")
+
+    # Return the template as JSON
+    return jsonify(id=template.id, template_text=template.template_text)
 
 
 @app.errorhandler(404)
 def not_found(e):
+    '''
+    Error handler for 404 errors
+    '''
     return jsonify(error=str(e.description)), 404
---
+++ backend/initialize_db.py
@@ -0,0 +1,48 @@
+from app import db, Template
+
+# Create the database and the tables
+db.create_all()
+
+sample_template_text = """
+EXAM:
+CT [_laterality_] [Generic Bone], [without or with] IV contrast
+
+CLINICAL HISTORY:
+
+TECHNIQUE:
+Axial images were acquired through the [_laterality_] [generic bone]
+[without or with] IV contrast. Reformatted images were reviewed.
+Dose reduction technique was used including one or more of the following:
+automated exposure control, adjustment of mA and kV according to patient size,
+and/or iterative reconstruction.
+
+COMPARISON:
+None provided.
+
+FINDINGS:
+
+BONES:
+No acute fracture or focal osseous lesion.
+
+JOINTS:
+No dislocation. The joint spaces are normal.
+
+SOFT TISSUES:
+The soft tissues are unremarkable.
+
+IMPRESSION:
+
+1. No acute osseous abnormality.
+"""
+
+# Check if sample data already exists
+existing_template = db.session.query(Template).first()
+if not existing_template:
+    # Add sample templates
+    sample1 = Template(template_text=sample_template_text)
+
+    db.session.add(sample1)
+    db.session.commit()
+    print("Added sample template")
+else:
+    print("Sample template already exists")
