d827dd6cc3f367dd8e847a9d1c363f37616e3af9
Ansh Dubey <ansh@ualberta.ca>
2023-10-29 05:01:07 -0600

    chore: add basic routes + error handling

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,12 @@
 from flask import Flask, jsonify, abort
+from werkzeug.exceptions import NotFound
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return jsonify({'message': 'Hello World!'})
 
 
 @app.route('/get-template/<int:template_id>', methods=['GET'])
