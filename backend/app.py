971eaf900745a6296d0e8d43b984e1937606d2a9
Ansh Dubey <ansh@ualberta.ca>
2023-10-31 01:22:11 -0600

    feat: dockerfiles finally working 🔥

--- backend/app.py
+++ backend/app.py
@@ -48,3 +48,6 @@ def not_found(e):
     Error handler for 404 errors
     '''
     return jsonify(error=str(e.description)), 404
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
